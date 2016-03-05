3d5bf16d8026b4c2a869957becce92a39928b207
Martin Kunz <martin.michael.kunz@gmail.com>
2016-03-05 19:23:55 +0100

    234

---
+++ libs/models/trees/TwoThreeFour.js
@@ -0,0 +1,255 @@
+/*
+ Software License Agreement (BSD License)
+ http://wwwlab.cs.univie.ac.at/~a1100570/webAD/
+ Copyright (c), Volodimir Begy
+ All rights reserved.
+
+
+ Redistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following condition is met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+var N=2;
+function Node(){
+	this.values = [];
+	this.children = [];
+	this.parent = undefined;
+	this.xPosition = 0;
+	this.yPosition = 0;
+
+	this.isLeaf=function(){
+		return this.children.length==0;
+	}
+
+	this.split=function()
+	{
+		var parent=new Node();
+		var right=new Node();
+		right.values=this.values.slice(N+1);
+		right.children=this.children.slice(N)
+		parent.children.push(this,right);
+		parent.values.push(this.values[N]);
+		this.values=this.values.slice(0,N);
+		this.children=this.children.slice(0,N);
+		return parent;
+	}
+
+	this.insertIndex=function(value, node) {
+
+		var idx=0;
+		while(value > this.values[idx]&&idx<this.values.length)
+			idx++;
+
+		if(node!=null)
+		{
+			this.values.splice(idx,0,node.values[0]);
+			this.children.splice(idx+1, 0, node.children[1]);
+		}
+		else
+		{
+			this.values.splice(idx,0,value);
+		}
+
+	}
+
+	this.print=function()
+	{
+		var txt=" ";
+		for(var i=0;i<this.values.length;i++)
+			txt+=","+this.values[i];
+		console.log(txt)
+	}
+
+	this.getSize=function()
+	{
+		return this.values.length+1;
+	}
+}
+
+function TwoThreeFour(){
+	this.view=new TwoThreeFourView(this);
+	this.db=[];
+	this.root=undefined;
+	this.actStateID=-1;
+	this.N=2; //234 Tree
+}
+
+TwoThreeFour.prototype.init=function(){
+	this.saveInDB();
+}
+
+TwoThreeFour.prototype.copy=function(toCopy){
+	var newTree=new TwoThreeFour();
+			
+	function recursivePreorderTraversal(newTree,node){
+		if(node==undefined)
+			return;
+		
+		newTree.addFixed(node.value);
+		recursivePreorderTraversal(newTree,node.leftChild);
+		recursivePreorderTraversal(newTree,node.rightChild);
+	}
+
+	recursivePreorderTraversal(newTree,toCopy.root);
+
+	return newTree;
+}
+
+TwoThreeFour.prototype.replaceThis=function(toCopy){
+	
+	this.root=undefined;
+	function recursivePreorderTraversal(tree,node){
+		if(node==undefined)
+			return;
+		
+		tree.addFixed(node.value);
+		recursivePreorderTraversal(tree,node.leftChild);
+		recursivePreorderTraversal(tree,node.rightChild);
+	}
+
+	recursivePreorderTraversal(this,toCopy.root);
+
+}
+
+TwoThreeFour.prototype.prev=function(){
+}
+
+TwoThreeFour.prototype.next=function(){
+}
+
+TwoThreeFour.prototype.firstState=function(){
+}
+
+TwoThreeFour.prototype.lastState=function(){
+}
+
+TwoThreeFour.prototype.insertNode=function(node, value) {
+	var newNode=null;
+	for(var i=0;i<node.children.length;i++) {
+		if (i==node.values.length||node.values[i] > value) {
+			newNode=this.insertNode(node.children[i], value);
+			break;
+		}
+	}
+
+	if(node.children.length==0) //unterste ebene
+	{
+		node.insertIndex(value);
+		if(node.values.length>this.N*2-1) //noch platz im index
+		{
+			newNode=node.split();
+			if(newNode!=null&&node==this.root)
+			{
+				this.root=newNode;
+			}
+			return newNode;
+		}
+		return null;
+	}
+
+	if (newNode == null)
+		return newNode;
+
+	if (node.getSize() < this.N * 2) {//noch platz im index
+		node.insertIndex(value, newNode);
+		return;
+	}
+
+	newNode=node.split();
+	if(node==this.root)
+		this.root=newNode;
+	return newNode;
+}
+
+TwoThreeFour.prototype.add=function() {
+	var val=parseInt(prompt("Add:"));
+	if(isNaN(val))return;
+	var node=new Node();
+	node.value=val;
+		
+	if(this.root==undefined){
+		this.root=node;
+		this.draw();
+		//this.saveInDB();
+	}
+	this.insertNode(this.root,val);
+	this.root.print();
+	this.draw();
+}
+
+TwoThreeFour.prototype.addFixed=function(_val) {
+	var node=new Node();
+	
+	node.value=_val;
+		
+	if(this.root==undefined){
+		this.root=node;
+	}
+}
+
+TwoThreeFour.prototype.saveInDB=function(){
+}
+
+TwoThreeFour.prototype.search=function() {
+
+	var value=parseInt(prompt("Search for:"));
+	if(isNaN(value))return;
+	
+	var tree=this;
+	
+	if(tree.root==undefined){
+		return;
+	}
+
+
+}
+
+
+TwoThreeFour.prototype.remove=function() {
+
+	var value=parseInt(prompt("delete:"));
+	if(isNaN(value))return;
+	if(this.root==undefined){
+		return;
+	}
+
+}
+
+TwoThreeFour.prototype.random=function(){
+	//var count=this.db().count();
+
+	//if(count==this.actStateID){
+		
+		this.root=undefined;
+		var number=parseInt(Math.random()*10,10); 
+	
+		for(var i=0;i<number;i++){
+			this.addFixed(parseInt(Math.random()*1000,10));
+		}
+		
+		this.saveInDB();
+		this.draw();
+	
+	//}
+}
+
+TwoThreeFour.prototype.example=function(){
+	this.root=undefined;
+	var numbers=[5,3,10,12,1,6];
+
+	for(var i=0;i<numbers.length;i++){
+		this.addFixed(numbers[i]);
+	}
+		
+	this.saveInDB();
+	this.draw();
+}
+
+TwoThreeFour.prototype.draw=function(){
+	this.view.draw();
+}
+
---
+++ libs/views/trees/TwoThreeFour.js
@@ -0,0 +1,96 @@
+/*
+ Software License Agreement (BSD License)
+ http://wwwlab.cs.univie.ac.at/~a1100570/webAD/
+ Copyright (c), Volodimir Begy
+ All rights reserved.
+
+
+ Redistribution and use of this software in source and binary forms, with or without modification, are permitted provided that the following condition is met:
+
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+function TwoThreeFourView(_model){
+	this.model=_model;
+	this.scale=1;
+}
+
+TwoThreeFourView.prototype.initStage=function(cont){
+	this.stage = new Kinetic.Stage({
+		container: cont,
+		draggable: true,
+		width: 0,
+		height: 0
+	});
+}
+
+TwoThreeFourView.prototype.zoomIn=function(){
+	if(this.scale<2.5)this.scale=this.scale+0.1;
+	this.draw();
+}
+
+TwoThreeFourView.prototype.zoomOut=function(){
+	if(this.scale>0.5)this.scale=this.scale-0.1;
+	this.draw();
+}
+
+TwoThreeFourView.prototype.draw=function(){
+
+	var tmpNodes=[];
+	if(this.model.root!=undefined)
+		tmpNodes.push(this.model.root);
+	var level=1;
+	var finished=new Boolean();
+	finished=false;
+	var model=this.model;
+	this.stage.setHeight(500);
+	this.stage.setWidth(1000);
+	this.stage.removeChildren();
+
+	var layer = new Kinetic.Layer();
+
+	var row=1;
+	var col=0;
+	function bla(node, row, col,x,y)
+	{
+		for(var i=0;i<node.values.length;i++)
+		{
+			var textX=100+i*50+50*(col*Math.pow(model.N,row));
+			var textY=row*50+100;
+			var text = new Kinetic.Text({
+				y: textY,
+				x: textX,
+				text: ':'+node.values[i],
+				fontSize: 15,
+				fontFamily: 'Calibri',
+				fill: 'green'
+			});
+			layer.add(text);
+
+
+			var line = new Kinetic.Line({
+				points: [x, y, textX, textY],
+				stroke: 'red',
+				strokeWidth: 2,
+				lineCap: 'round',
+				lineJoin: 'round',
+				dashArray: [1, 1]
+			});
+			layer.add(line);
+		}
+
+		for(var i=0;i<node.children.length;i++)
+		{
+			bla(node.children[i], row+1, col+i, 100+i*50 +25*(col*Math.pow(model.N,row)), row*50+100);
+
+		}
+	}
+	bla(this.model.root,row,col);
+	//add to layer
+
+
+	this.stage.add(layer);
+}
